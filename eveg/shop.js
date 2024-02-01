9076e628d2284aea068351209ff8b76926a824f2
Amelia Moss <u2107732@dcs>
2024-02-01 16:35:07 +0000

    made button and input number of items prettier

--- eveg/shop.js
+++ eveg/shop.js
@@ -15,7 +15,7 @@ var cardTemplate = `<div class="shop-product card" data-num="[EVEGPRODUCT#]">
 <div class="shop-product-details shop-product-price" data-field="price" data-num="[EVEGPRODUCT#]"></div>
 <div class="shop-product-details shop-product-units" data-field="units" data-num="[EVEGPRODUCT#]"></div>
 <div class="shop-product-buying" data-num="[EVEGPRODUCT#]">
-<div class="productBasketDiv"><button class="addToBasket">Add to Basket</button>
+<div class="productBasketDiv"><button class="addToBasket">Add</button>
 <div class="adjustDiv"><button class="btn adjustDown">-</button>
 <input class="buyInput" data-num="[EVEGPRODUCT#]" min="0" value="0" type="number">
 <button class="btn adjustUp">+</button></div></div></div></div></div>`;
