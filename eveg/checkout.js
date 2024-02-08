b78bda7978d7809f09bae3ffdf0e208978cde150
Amelia Moss <u2107732@dcs>
2024-02-08 16:38:15 +0000

    input changes prices + added headers

--- eveg/checkout.js
+++ eveg/checkout.js
@@ -36,27 +36,59 @@ function calculateBasket(){
   let total = 0;
   let basket = JSON.parse(getCookie("basket"));
   document.querySelector('.checkoutList').innerHTML = '';
+  var headerRow = document.createElement("tr");
+  var itemHeader = document.createElement("th");
+  itemHeader.textContent = "Items";
+  itemHeader.colSpan = 2;
+  var quantHeader = document.createElement("th");
+  quantHeader.textContent = "Quantity";
+  var itemPriceHeader = document.createElement("th");
+  itemPriceHeader.textContent = "Price per Item";
+  var totalHeader = document.createElement("th");
+  totalHeader.textContent = "Item Total";
+  headerRow.appendChild(itemHeader);
+  headerRow.appendChild(quantHeader);
+  headerRow.appendChild(itemPriceHeader);
+  headerRow.appendChild(totalHeader);
+  document.querySelector('.checkoutList').appendChild(headerRow);
   for(const productID in basket){
+    console.log(productID);
     let quantity = basket[productID];
     let price = productDetails[productID].price;
     let productTotal = price * quantity;
     total = total + productTotal;
+    let totalID = productID + "total";
     // let rowHTML = `<td>${productDetails[productID].name}</td><td><input class="checkoutQuant"  min="0" value=${quantity} type="number"></input></td><td>${(price / 100).toFixed(2)}</td><td>£${(productTotal / 100).toFixed(2)}</td>`;
     // Include the image in the rowHTML
     let rowHTML = `<td><img src="images/${productDetails[productID].image}" alt="${productDetails[productID].name}" style="width: 50px; height: 50px;"></td>
                <td>${productDetails[productID].name}</td>
-               <td><input class="checkoutQuant" min="0" value=${quantity} type="number"></input></td>
+               <td><input id = ${productID} class="checkoutQuant" min="0" value=${quantity} type="number" oninput=changeValue(${productID})> </input></td>
                <td>${(price / 100).toFixed(2)}</td>
-               <td>£${(productTotal / 100).toFixed(2)}</td>
+               <td id = ${totalID} class = "productTotal">£${(productTotal / 100).toFixed(2)}</td>
                `;
     var thisProduct = document.createElement("tr");
     thisProduct.innerHTML = rowHTML;
     document.querySelector('.checkoutList').appendChild(thisProduct);
   }
-  let rowHTML = `<td colspan="3">Total:</td><td>£${(total / 100).toFixed(2)}</td>`;
+  let rowHTML = `<td colspan="3" id = "totalPrice"> Total:</td><td>£${(total / 100).toFixed(2)}</td>`;
   var thisProduct = document.createElement("tr");
+  thisProduct.id = "totalRow";
   thisProduct.innerHTML = rowHTML;
   document.querySelector('.checkoutList').appendChild(thisProduct);
 }
 
 window.addEventListener("load", init);
+
+function changeValue(ID){
+  var inputValue = document.getElementById(ID).value;
+  document.getElementById(ID + "total").innerText = ((inputValue * productDetails[ID].price)/100).toFixed(2);
+  var total = document.getElementById("totalRow");
+
+  var inputs = document.getElementsByClassName("checkoutQuant");
+      var totalPrice = 0;
+      for (var i = 0; i < inputs.length; i++) {
+        totalPrice += parseFloat(inputs[i].value * productDetails[inputs[i].id].price);
+      }
+  total.innerHTML = "";
+  total.innerHTML = `<td colspan="3" id = "totalPrice"> Total:</td><td>£${(totalPrice/100).toFixed(2)}</td>`;
+}
